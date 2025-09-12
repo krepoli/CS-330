04c234baeb30769cd2dc82495f68d6c13727a7b9
Katie Repoli <168455578+krepoli@users.noreply.github.com>
2025-09-11 20:05:27 -0400

    print hello, world

---
+++ helloworld.cpp
@@ -0,0 +1,12 @@
+// code from https://www.w3schools.com/cpp/cpp_syntax.asp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x;
+  cout << "Hello World!";
+  cin >> x;
+  cout << x;
+  return 0;
+}
