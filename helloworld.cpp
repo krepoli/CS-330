ebb352b1234f5e4d0e17ac986477238125352d84
Katie Repoli <168455578+krepoli@users.noreply.github.com>
2025-10-13 17:42:17 -0400

    print, hello world

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,6 @@
 // code from https://www.w3schools.com/cpp/cpp_syntax.asp
 
-// this is a header file library that allows input and output objects
 #include <iostream>
-// this means we can use names for objects and variables
 using namespace std;
   
 int main() { // starts the function
