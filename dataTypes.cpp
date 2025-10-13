60699017978461758ab22e4c471eae047ff3637f
Katie Repoli <168455578+krepoli@users.noreply.github.com>
2025-10-13 17:43:23 -0400

    data types

--- dataTypes.cpp
+++ dataTypes.cpp
@@ -4,52 +4,52 @@ using namespace std;
 int main() {
     // integer
     int num = 10;
-    cout << num << "\n";
+    cout << num << endl;
 
     // floating point (decimal)
     float floating = 10.25;
-    cout << num << "\n";
+    cout << num << endl;
 
     // double (precision floating point)
     double e = 2.71828;
-    cout << e << "\n";
+    cout << e << endl;
 
     // boolean
     bool isTrue = true;
-    cout << isTrue << "\n"; // prints 1
+    cout << isTrue << endl; // prints 1
     bool isFalse = false;
-    cout << isFalse << "\n"; // prints 0
+    cout << isFalse << endl; // prints 0
 
     // character (stores a single character)
     char c = 'C';
-    cout << c << "\n";
+    cout << c << endl;
 
     // string
     string world = "Hello, World!";
-    cout << world << "\n";
+    cout << world << endl;
 
     /* pointer:
     holds the memory address of another variable */
     int* pointer = &num;
-    cout << "Value of num: " << num << "\n";
-    cout << "Address of num: " << &num << "\n";
-    cout << "Value Stored in pointer: " << pointer << "\n";
-    cout << "Value Pointed to by pointer: " << *pointer << "\n";
+    cout << "Value of num: " << num << endl;
+    cout << "Address of num: " << &num << endl;
+    cout << "Value Stored in pointer: " << pointer << endl;
+    cout << "Value Pointed to by pointer: " << *pointer << endl;
 
     // arrays - you must always establish the size of an array
     string languages[3] = {"Python", "Java", "C"};
-    cout << languages[2] << "\n";
+    cout << languages[2] << endl;
     languages[2] = "C++";
-    cout << languages[2] << "\n";
+    cout << languages[2] << endl;
 
 
-    // mathematical operations
+    // mixed type operations
     // recall: num = 10
 
-    cout << num + 5 << "\n"; // int + int
-    cout << num + floating << "\n"; // int + float
-    cout << num * e << "\n"; // int * double
-    cout << num << "5" << "\n"; // int + string
+    cout << num + 5 << endl; // int + int
+    cout << num + floating << endl; // int + float
+    cout << num * e << endl; // int * double
+    cout << num << "5" << endl; // int + string
 
     return 0;
 }
